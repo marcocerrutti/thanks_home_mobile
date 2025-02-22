e301a267a7411e7e005945fcebb4bc2d73da8370
Uchenna Nwaigwe <hermessuffice@gmail.com>
2025-02-22 20:57:57 +0100

    Fundamentals

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  Button,
+  Platform,
+  SafeAreaView,
+  StyleSheet,
+  StatusBar,
+} from "react-native";
 
 export default function App() {
-  console.log("App  executed!");
   return (
-    <View style={styles.container}>
-      <Text>Hello React</Text>
-    </View>
+    <SafeAreaView style={{ backgroundColor: "orange" }}>
+      <Button
+        color="orange"
+        title="Click Me"
+        onPress={() => {
+          "Button Tapped!";
+        }}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "black",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
 });
